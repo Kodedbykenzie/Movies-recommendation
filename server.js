fde0d5c092d7498c73b1894177b47974257c626c
Precious Mozia <chibundumozia@gmail.com>
2024-11-28 15:53:20 +0200

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html when visiting the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Set the port to listen on
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
